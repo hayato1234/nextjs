7ca83f3458a4b1bcbd957836b5379ab1af401e04
Steve Moritani <steve.hayato.m@gmail.com>
2024-02-04 18:27:26 -0800

    Add skeleton for loading

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -30,6 +30,10 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
   noStore();
+  console.log('Fetching revenue data...');
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
+  console.log('Data fetch completed after 1 second.');
   try {
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
